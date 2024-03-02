f9d847adf55498eec6c66d030bb595e82ecaa99a
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-03-02 15:40:48 -0300

    Se agregó un nuevo contenedor en ListCoffeeContainer.jsx para que contenga un vector y otro contenedor dentro que contiene el titulo y un texto.

--- src/ListCoffeeContainer.jsx
+++ src/ListCoffeeContainer.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./ListCoffeeContainer.css";
+import Parrafos from "./Parrafos.jsx";
 function Listcoffeecontainer() {
   return (
     <div className="list-coffee-container">
       <div className="article-container">
-        <h1>Our Collection</h1>
-        <p>
-          Introducing our Coffee Collection, a selection of unique coffees from
-          different roast types and origins, expertly roasted in small batches
-          and shipped fresh weekly.
-        </p>
+        <div className="article-content">
+          <h1 className="article-content__title">Our Collection</h1>
+          <p className="article-content__paragraph">
+            Introducing our Coffee Collection, a selection of unique coffees
+            from different roast types and origins, expertly roasted in small
+            batches and shipped fresh weekly.
+          </p>
+        </div>
       </div>
     </div>
   );
