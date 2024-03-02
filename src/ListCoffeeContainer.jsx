7b1b617c8feccb224ff5804a39a2bfc7c2244f78
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-03-02 14:39:16 -0300

    Se creó un nuevo componente llamado: ListCoffeeContainer.jsx al cual se le dieron estilos.

---
+++ src/ListCoffeeContainer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ListCoffeeContainer.css";
+function Listcoffeecontainer() {
+  return (
+    <div className="list-coffee-container">
+      <div className="article-container">
+        <h1>Our Collection</h1>
+        <p>
+          Introducing our Coffee Collection, a selection of unique coffees from
+          different roast types and origins, expertly roasted in small batches
+          and shipped fresh weekly.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Listcoffeecontainer;
