64442bf213d9333c98339c29cfdba82818b871c9
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-03-07 19:57:30 -0300

    Se modificó el tamaño de las imagenes y se les dió mas estilos.

--- src/CoffeePriceCard.jsx
+++ src/CoffeePriceCard.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./CoffeePriceCard.css";
-function CoffeePriceCard({ id, title, image }) {
+function CoffeePriceCard({ id, title, image, price, delimited }) {
   return (
     <div key={id}>
-      <h3>{title}</h3>
-      <img src={image} width={200} height={200} />
+      <img
+        src={image}
+        width={300}
+        height={260}
+        style={{ borderRadius: "20px", objectFit: "cover" }}
+      />
+
+      <div className="coffee-type--price">
+        <h3 className="coffee-type">{title}</h3>
+        <p className="coffee-price">${price}</p>
+      </div>
+      <div className="container-types__coffees">
+        <p className="coffe-votes">
+          4.85 <span>(14 votes)</span>
+          <p className="sold-out">{delimited}</p>
+        </p>
+      </div>
     </div>
   );
 }
