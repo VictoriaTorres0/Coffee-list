b1f2707967d61b62bacd669635885607b4bce173
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-03-07 11:34:43 -0300

    Se borraron Divs y decidí usar el componente como reutilizable varias veces.

--- src/CoffeePriceCard.jsx
+++ src/CoffeePriceCard.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import "./CoffeePriceCard.css";
-function CoffeePriceCard() {
+function CoffeePriceCard({ id, title, image }) {
   return (
-    <div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div key={id}>
+      <h3>{title}</h3>
+      <img src={image} width={200} height={200} />
     </div>
   );
 }
