ef80ac86e261669ce53dd72226c297877efebab7
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-03-11 14:16:26 -0300

    se utilizó una expresión ternaria para agregar y quitar una clase de un boton. Y se usó Filter para filtar algunas propiedades del objeto coffee.

--- src/ListCoffeeContainer.jsx
+++ src/ListCoffeeContainer.jsx
@@ -1,19 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./ListCoffeeContainer.css";
 import "./CoffeePriceCard.jsx";
 import CoffeePriceCard from "./CoffeePriceCard.jsx";
 function Listcoffeecontainer() {
   const [coffees, setCoffees] = React.useState([]);
+  const [filtrado, setFiltrado] = React.useState([]);
+  const [opcion, setOpcion] = React.useState("");
 
   const ObtenerCoffees = async () => {
-    const respuesta = await fetch("https://api.sampleapis.com/coffee/hot");
+    const respuesta = await fetch(
+      "https://65ece95f0ddee626c9b10814.mockapi.io/api/coffe"
+    );
     const resultado = await respuesta.json();
     setCoffees(resultado);
+    setFiltrado(resultado);
+  };
+
+  const CoffeeFilter = () => {
+    const disponibles = coffees.filter((cafe) => cafe.sold_out === false);
+    setFiltrado(disponibles);
+    setOpcion("boton-2");
+  };
+
+  const todosLosCafes = () => {
+    setFiltrado(coffees);
+    setOpcion("boton-1");
   };
 
   useEffect(() => {
     ObtenerCoffees();
   }, []);
+
   return (
     <div className="list-coffee-container">
       <div className="article-container">
@@ -25,20 +42,37 @@ function Listcoffeecontainer() {
             batches and shipped fresh weekly.
           </p>
           <div className="content-container-button">
-            <button className="article-content__button">All Products</button>
-            <button className="article-content__button">Available Now</button>
+            <button
+              className={`article-content__button ${
+                opcion === "boton-1" ? "botonSeleccionado" : ""
+              }`}
+              onClick={() => todosLosCafes()}
+            >
+              {" "}
+              All Products
+            </button>
+            <button
+              className={`article-content__button ${
+                opcion === "boton-2" ? "botonSeleccionado" : ""
+              }`}
+              onClick={() => CoffeeFilter()}
+            >
+              Available Now
+            </button>
           </div>
         </div>
       </div>
       <div className="Card-container">
-        {coffees.map((coffee) => (
+        {filtrado.map((coffee) => (
           <CoffeePriceCard
             title={coffee.title}
             image={coffee.image}
             id={coffee.id}
-            // price={coffee.price}
-            price={5.0}
-            delimited={"sold out"}
+            price={coffee.price}
+            delimited={coffee.sold_out}
+            popular={coffee.popular}
+            rating={coffee.rating}
+            votes={coffee.votes}
           />
         ))}
       </div>
